2ef3e96994ab5cdc91241ee5122a1b0aeac68f41
AJIJOGJA <haryadi@jogjacamp.co.id>
2018-05-17 16:32:11 +0700

    django rest framewor :: update

--- latihan/latihanapi/admin.py
+++ latihan/latihanapi/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import PostCategory, Post
 
-# Register your models here.
+
+class PostCategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('nama_kategori', 'deskripsi_kategori', 'created_by', 'updated_at')
+    # actions = notne
+
+    def save_model(self, request, obj, form, change):
+        if form.is_valid():
+            stock = form.save(commit=False)
+            stock.created_by = request.user
+            import datetime
+            stock.updated_at = datetime.datetime.now()
+            stock.save()
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('nama_barang', 'kode_barang', 'kategori', 'created_by', 'updated_at')
+    list_select_related = ('kategori', 'created_by')
+    # actions = notne
+
+    def save_model(self, request, obj, form, change):
+        if form.is_valid():
+            stock = form.save(commit=False)
+            stock.created_by = request.user
+            import datetime
+            stock.updated_at = datetime.datetime.now()
+            stock.save()
+            suffix = stock.nama_barang.replace(' ', '')
+            suffix = suffix.decode('utf-8').lower()
+            stock.kode_barang = '{0}.{1}.{2}.{3}' . format(suffix[:3], stock.created_by_id, stock.kategori_id, stock.id)
+            stock.save()
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(created_by=request.user)
+
+    # def has_change_permission(self, request, obj=None):
+    #     if not obj:
+    #         # the changelist itself
+    #         return True
+    #     return obj.user == request.user
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
