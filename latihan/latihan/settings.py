4437de1b2883ed25796e489681804028459bdcf2
AJIJOGJA <haryadi@jogjacamp.co.id>
2018-08-09 10:22:15 +0700

    archive

--- latihan/latihan/settings.py
+++ latihan/latihan/settings.py
@@ -126,9 +126,12 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticated',#The default permission policy may be set globally
-        #'rest_framework.permissions.AllowAny', # this setting defaults to allowing unrestricted access
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+
+        # The default permission policy may be set globally
+        'rest_framework.permissions.IsAuthenticated',
+
+        # 'rest_framework.permissions.AllowAny', # this setting defaults to allowing unrestricted access
     ]
 }
 
