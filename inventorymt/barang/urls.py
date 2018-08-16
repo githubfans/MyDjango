31b50d1d73c29b83694f11f6073082503f38ed70
AJIJOGJA <haryadi@jogjacamp.co.id>
2018-08-16 09:39:51 +0700

    update

--- inventorymt/barang/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-
-from barang import views
-
-urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('^admin/', admin.site.urls),
-]
--- tenantsx/tenants/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from tenant_schemas.models import TenantMixin
-
-
-class Tenant(TenantMixin):
-    name = models.CharField(max_length=100)
-
-    auto_create_schema = True
