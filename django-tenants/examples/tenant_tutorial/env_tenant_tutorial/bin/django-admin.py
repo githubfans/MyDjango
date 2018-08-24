fcb2adba1ea8ea77f38581d3e0a473d9a9574c8c
AJIJOGJA <haryadi@jogjacamp.co.id>
2018-08-24 14:48:14 +0700

    update

--- django-tenants/examples/tenant_tutorial/env_tenant_tutorial/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/sysadmin/Documents/belajar_python/Django/django-tenants/examples/tenant_tutorial/env_tenant_tutorial/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
